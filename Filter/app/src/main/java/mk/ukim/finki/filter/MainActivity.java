4ba361be76e0a3fd6558242f5aff1beb0deaafe2
spaunova <misspaunova@gmail.com>
2016-09-14 00:36:36 +0200

    Add basic filters

--- Filter/app/src/main/java/mk/ukim/finki/filter/MainActivity.java
+++ Filter/app/src/main/java/mk/ukim/finki/filter/MainActivity.java
@@ -12,14 +12,30 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
 public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2{
+    private static final int CANNY_THRESHOLD = 150;
+    // threshold value for canny hysteresis
+    private static final int THRESHOLD_DIFFERENCE = 20;
+
+    private static final int NORMAL = 1;
+    private static final int GRAYSCALE = 2;
+    private static final int EDGE = 3;
+    private static final int BLUR = 4;
+    private static final int SHARPEN = 5;
+    private int selectedView = NORMAL;
 
     private CameraBridgeViewBase mOpenCvCameraView;
 
@@ -41,6 +57,47 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+        initButtons();
+    }
+
+    private void initButtons() {
+        Button normal = (Button)findViewById(R.id.btn_filter_normal);
+        normal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedView = NORMAL;
+            }
+        });
+        Button gray = (Button)findViewById(R.id.btn_filter_gray);
+        gray.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedView = GRAYSCALE;
+            }
+        });
+        Button edge = (Button)findViewById(R.id.btn_filter_edge);
+        edge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedView = EDGE;
+            }
+        });
+        Button blur = (Button)findViewById(R.id.btn_filter_blur);
+        blur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedView = BLUR;
+            }
+        });
+        Button sharp = (Button)findViewById(R.id.btn_filter_sharp);
+        sharp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedView = SHARPEN;
+            }
+        });
+
     }
 
     @Override
@@ -116,6 +173,34 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        if (selectedView == GRAYSCALE) {
+            Log.d("SELECTED_VIEW", "GRAYSCALE");
+            return inputFrame.gray();
+        } else if (selectedView == EDGE) {
+            Log.d("SELECTED_VIEW", "EDGE");
+            Mat gray = inputFrame.gray();
+            Mat result = new Mat(gray.size(), gray.type());
+            Imgproc.Canny(gray, result, CANNY_THRESHOLD, CANNY_THRESHOLD + THRESHOLD_DIFFERENCE);
+            return result;
+        } else if (selectedView == BLUR) {
+            Log.d("SELECTED_VIEW", "BLUR");
+            Mat rgba = inputFrame.rgba();
+            Mat result = new Mat(rgba.size(), rgba.type());
+            Imgproc.blur(rgba, result, new Size(7, 7));
+            return result;
+        } else if (selectedView == SHARPEN) {
+            Log.d("SELECTED_VIEW", "SHARPEN");
+            Mat img = inputFrame.rgba();
+            Mat sharpened = new Mat();
+            Imgproc.filter2D(img, sharpened, -1, new MatOfInt(
+                     0, -1,  0,
+                    -1,  5, -1,
+                     0, -1,  0
+            ));
+            return sharpened;
+        }
+
+        Log.d("SELECTED_VIEW", "NORMAL");
         return inputFrame.rgba();
     }
 }
